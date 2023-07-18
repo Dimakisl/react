75c0abed92d424220a7711056797d4c7f3717834
Dimakisl <39777576+Dimakisl@users.noreply.github.com>
2023-07-18 22:32:13 +0300

    add assembly

---
+++ src/module.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Alissa from './img/Alissa.jpg';
+
+const Module = () => (
+    <>
+        <h1>hello, world</h1>
+        <img src={Alissa} alt="" />
+    </>
+);
+
+export default Module;
+
